e7d2992915e36501870927bd11b32661da182589
shuaib shaban <shuaib>
2023-02-08 15:30:33 +0300

    passed tests

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarf_names)# code an argument here
   # Your code here
+  dwarf_names.each_with_index do |name, index|
+    puts "#{index + 1}. #{name}"
+    end
+end
+ roll_call_dwarves(["Doc", "Dopey", "Bashful", "Grumpy"])
+
+def summon_captain_planet(planeteer_calls)# code an argument here
+  # Your code here
+  planeteer_calls.map { |call| call.capitalize + "!"}
 end
 
-def summon_captain_planet# code an argument here
+def long_planeteer_calls(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.any? { |call| call.length > 4}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def find_the_cheese(ingredients)
+  cheeses = ["cheddar", "gouda", "camembert"]
+  ingredients.find { |ingredient| cheeses.include?(ingredient) }
 end
+ingredients = ["garlic", "rosemary", "bread"]
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
